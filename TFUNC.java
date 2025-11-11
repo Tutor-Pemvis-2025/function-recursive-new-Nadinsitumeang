94256041485fdb673a5a0ccbf36c7e7032d6fd09
Nadinsitumeang <nadinsitumeang03@gmail.com>
2025-11-11 21:08:30 +0700

    Nadin &Miransya
    donee

--- TFUNC.java
+++ TFUNC.java
@@ -3,15 +3,16 @@
 import java.util.*;
 import java.lang.Math;
 
-public class Hitungrekursif {
+public class  TFUNC{
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int n;
+        int n, result;
 
         n = Integer.parseInt(input.nextLine());
-        n = hitungrekursif(n);
-        System.out.println(n);
+        result = hitungrekursif(n);
+        System.out.println("Bilangan yang dimasukkan : " + n);
+        System.out.println("Hasil perhitungan rekursif : " + result);
     }
     
     public static int hitungrekursif(int n) {
@@ -26,3 +27,5 @@ public class Hitungrekursif {
         return result;
     }
 }
+
+
